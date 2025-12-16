a02baa2063ae7e4c22b55958233d3363eadca440
Michael Kamb <michael.kamb@yahoo.com>
2025-12-15 21:50:34 -0600

    Use St. Paul v2: Bibliocommons parity with Hennepin
    - Replace table selectors with div based parsing (identical to Hennepin)
    - Add s_c_v2.html, s_h_v2.html fixtures from new site
    - Remove obsolete s_c.html, s_h.html fixtures
    - Update mock_expected for v2 output (nearly same text)

--- lib/stpaul.rb
+++ lib/stpaul.rb
@@ -1,5 +1,5 @@
 module StPaul
-  STPAUL_BASE_URL_actual = "https://alpha.stpaul.lib.mn.us"
+  STPAUL_BASE_URL_actual = "https://sppl.bibliocommons.com"
   STPAUL_BASE_URL_local = "http://localhost:3000/stpaul"
 
   STPAUL_BASE_URL = Local.local? ? STPAUL_BASE_URL_local : STPAUL_BASE_URL_actual
@@ -7,84 +7,88 @@ module StPaul
   def self.lib_data
     {
       post_data: Secret::POST_DATA,
-      post_url: STPAUL_BASE_URL + "/patroninfo~S16",
-      checked_out_url: STPAUL_BASE_URL + "/patroninfo~S16/1600472/items",
-      holds_url: STPAUL_BASE_URL + "/patroninfo~S16/1600472/holds",
+      post_url: STPAUL_BASE_URL + "/user/login?%2Fuser_dashboard",
+      checked_out_url: STPAUL_BASE_URL + "/v2/checkedout",
+      holds_url: STPAUL_BASE_URL + "/v2/holds/ready_for_pickup",
       print_name: "St. Paul",
-      checked_out_fixture: "s_c.html",
-      holds_fixture: "s_h.html",
+      checked_out_fixture: "s_c_v2.html",
+      holds_fixture: "s_h_v2.html",
       trace_name: "StPaul",
     }
   end
 
   def self.parse_checkedout_page(page)
-    doc = Nokogiri.parse(page)
-    books_out = doc.css("table tr.patFuncEntry").map do |record|
-      book_record = CheckedOutRecord.new
-      book_record.title = finds_either_title(record)
-      td_status = record.css("td.patFuncStatus").first
-      status = td_status.text.strip
-      date_match = status.match(
-        /^.* ([0-9]{2})-([0-9]{2})-([0-9]{2}).*$/
-      )
-      book_record.due_date = Date.new(
-        "20#{date_match[3]}".to_i, date_match[1].to_i, date_match[2].to_i
-      )
-      # status is word DUE, date, times renewed (what if it's over due? prolly have a date anyway)
-      # puts status
-      # puts due_date.strftime("%A %B %d, %Y")
-      renewed_span = record.css("td.patFuncStatus span.patFuncRenewCount").first
-      renewed_count_text = "Renewed 0 times"
-      if renewed_span
-        renewed_count_text = renewed_span.text.strip
-      end
-      renewed_count_match = renewed_count_text.match(
-        /^Renewed ([0-9]{1}) times*$/ # *s for time or times possibly appearing
-      )
-      book_record.renewed_count = renewed_count_match[1].to_i
-      book_record.number_waiting = 0
-      book_record
+    stpaul_checked_out_books_page = Nokogiri.parse(page)
+    books_out = stpaul_checked_out_books_page.css("div.cp-checked-out-item").map do |book_part|
+      the_title = find_title(book_part)
+      the_date = find_date(book_part)
+      the_renewed_count = find_renewed_count(book_part)
+      the_number_waiting = find_number_waiting(book_part)
+      CheckedOutRecord.new(the_title, the_date, the_renewed_count, the_number_waiting)
     end
-    books_out.sort { |a, b|
-      (a.due_date == b.due_date) ?
-        a.title.downcase.sub(/^(the|a|an)\s+/i, "") <=> b.title.downcase.sub(/^(the|a|an)\s+/i, "") :
-        a.due_date <=> b.due_date
-    }
+    books_out.sort { |a, b| Recs.checked_compare(a, b) }
   end
 
   def self.parse_on_hold_page(page)
-    doc = Nokogiri.parse(page)
-    books_on_hold = doc.css("table tr.patFuncEntry").map do |record|
-      td_status = record.css("td.patFuncStatus").first
-      status = td_status.text.strip
-      if status.match(/^Ready/)
-        book_record = OnHoldRecord.new
-        book_record.title = finds_either_title(record)
-        date_match = status.match(
-          /^.* ([0-9]{2})-([0-9]{2})-([0-9]{2}).*$/
-        )
-        book_record.exp_date = Date.new(
-          "20#{date_match[3]}".to_i, date_match[1].to_i, date_match[2].to_i
-        )
-        book_record
-      end
-    end.compact
-    books_on_hold.sort { |a, b|
-      (a.exp_date == b.exp_date) ?
-        a.title.downcase.sub(/^(the|a|an)\s+/i, "") <=> b.title.downcase.sub(/^(the|a|an)\s+/i, "") :
-        a.exp_date <=> b.exp_date
-    }
+    stpaul_on_hold_books_page = Nokogiri.parse(page)
+    books_on_hold = stpaul_on_hold_books_page.css("div.cp-bib-list-item.cp-hold-item.ready_for_pickup").map do |book_part|
+      the_title = find_on_hold_title(book_part)
+      the_date = find_on_hold_date(book_part)
+      OnHoldRecord.new(the_title, the_date)
+    end
+    books_on_hold.sort { |a, b| Recs.holds_compare(a, b) }
   end
 
-  def self.finds_either_title(book_part)
-    # <th class="patFuncBibTitle" scope="row"><a href="/record=b1266537~S16">
-    # <span class="patFuncTitleMain">Feed zone portables : a cookbook et cetera / Biju Thomas & Allen Lim.</span></a><br />
-    # </th>
-    title_span = book_part.css("th.patFuncBibTitle span.patFuncTitleMain").first
-    # remove the author after a /  remove possible sub title after a :
-    # remove the author after an old style title [by] author sequence
-    title = title_span.inner_text.split(" /")[0].split(" :")[0].split(" [by]")[0]
-    title = title[0, 42]
-    title.sub(/\.$/, "")
+  def self.find_title(book_part)
+    # <a><span class="title-content">Introducing Elixir</span>
+    #   <span class="sr-only ">Introducing Elixir, Book</span></a>
+    book_part.css("a span.title-content").first.text
+  end
+
+  def self.find_date(book_part)
+    # <div class="cp-checked-out-due-on">
+    #   <span>Due by <span class="cp-short-formatted-date">Nov 19, 2018</span></span></div>
+    Date.parse book_part.css("div.cp-checked-out-due-on span.cp-short-formatted-date").first.text
+  end
+
+  def self.find_renewed_count(book_part)
+    # old <div class="cp-renew-count">
+    # old   <span>Renewed</span> <span>1 time</span></div>
+    # now
+    # <div class="cp-renew-count">Renewed 2 times</div>
+    renewed_count_div = book_part.css("div.cp-renew-count").first     # nokogiri element or nil
+    if renewed_count_div
+      n_times_text = renewed_count_div.text
+    else
+      n_times_text = "0 times"
+    end
+    n_times_text.match(/(\d+) times*/)[1].to_i
+  end
+
+  def self.find_number_waiting(book_part)
+    # old <div class="cp-held-copies-count">
+    # old  <span>1 person waiting</span></div>
+    # now
+    # <div class="cp-held-copies-count">27 people waiting</div>
+    number_waiting_div = book_part.css("div.cp-held-copies-count").first   # nokogiri element or nil
+    if number_waiting_div
+      n_people_waiting_text = number_waiting_div.text
+    else
+      n_people_waiting_text = "0 people waiting"
+    end
+    n_people_waiting_text.match(/(?<count_digits>\d+) (?:person|people) waiting/)[:count_digits].to_i
+  end
+
+  def self.find_on_hold_title(book_part)
+    # <a><span class="title-content">A History of America in Ten Strikes</span>
+    #   <span class="sr-only">A History of America in Ten Strikes, Book</span></a>
+    book_part.css("a span.title-content").first.text
+  end
+
+  def self.find_on_hold_date(book_part)
+    # <div class="holds-status ready_for_pickup">
+    #   <div class="cp-holds-secondary-info">
+    #     <span>Pick up by <span class="cp-short-formatted-date">Nov 14, 2018</span></span></div></div>
+    Date.parse book_part.css("div.holds-status.ready_for_pickup span.cp-short-formatted-date").first.text
   end
 end
